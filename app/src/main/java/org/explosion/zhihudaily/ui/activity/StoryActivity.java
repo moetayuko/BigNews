733a13e9457d4a4a41c3c72a2e76d64f353081e1
dianlujitao <dianlujitao@lineageos.org>
2017-05-09 14:49:25 +0800

    StoryActivity: Add Back and Share button to Toolbar

--- app/src/main/java/org/explosion/zhihudaily/ui/activity/StoryActivity.java
+++ app/src/main/java/org/explosion/zhihudaily/ui/activity/StoryActivity.java
@@ -1,10 +1,12 @@
 package org.explosion.zhihudaily.ui.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
@@ -30,6 +32,7 @@ public class StoryActivity extends AppCompatActivity {
     ImageView headImage;
     StoryContent storyContent;
     Toolbar toolbar;
+    net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout collapsingToolbarLayout;
 
     Context mContext;
 
@@ -41,8 +44,46 @@ public class StoryActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.story_toolbar);
         webView = (WebView) findViewById(R.id.story_web_view);
         headImage = (ImageView) findViewById(R.id.head_image);
+        collapsingToolbarLayout = (net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         mContext = getApplicationContext();
+
+        toolbar.setTitle(" ");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
         loadStory();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.toolbar_share:
+                if (storyContent != null) {
+                    String shareText = "";
+                    shareText += storyContent.getTitle() + " （分享自@知乎日报）" + storyContent.getShareUrl();
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+                    shareIntent.setType("text/plain");
+                    startActivity(shareIntent);
+                }
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadStory() {
@@ -57,11 +98,11 @@ public class StoryActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(String s, Call call, Response response) {
                         storyContent = new StoryContent(parseJSON.parseStoryContent(s));
+                        collapsingToolbarLayout.setTitle(storyContent.getTitle());
+
                         String html = WebUtils.buildHtmlWithCss(storyContent.getBody(), storyContent.getCss());
                         webView.loadDataWithBaseURL(null, html, WebUtils.MIME_TYPE, WebUtils.ENCODING, null);
                         Glide.with(mContext).load(storyContent.getImage()).into(headImage);
-                        toolbar.setTitle(storyContent.getTitle());
-                        setSupportActionBar(toolbar);
                     }
                 });
     }
